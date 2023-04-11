251c10160bf438a3d78a145f6cb145e7c97433d4
hyrious <hyrious@outlook.com>
2023-04-11 09:23:53 +0800

    try dangerous api

--- api/handler.ts
+++
@@ -1,5 +0,0 @@
-import type { VercelApiHandler } from '@vercel/node'
-
-export const handler: VercelApiHandler = async function handler(req, res) {
-  res.status(200).json({ body: req.body, query: req.query })
-}
